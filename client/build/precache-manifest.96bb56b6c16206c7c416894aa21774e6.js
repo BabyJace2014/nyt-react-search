cb2df361edf6dcb2e1d378864a1f001ea978652a
BabyJace2014 <35673340+BabyJace2014@users.noreply.github.com>
2018-10-11 17:38:15 -0400

    deploying

---
+++ client/build/precache-manifest.96bb56b6c16206c7c416894aa21774e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c139463edb76f8e6d35f",
+    "url": "/static/js/main.c139463e.chunk.js"
+  },
+  {
+    "revision": "104025ddd4c82cbb1d1d",
+    "url": "/static/js/1.104025dd.chunk.js"
+  },
+  {
+    "revision": "c139463edb76f8e6d35f",
+    "url": "/static/css/main.7b1de1c6.chunk.css"
+  },
+  {
+    "revision": "de99229d914151e15a649258d79094db",
+    "url": "/index.html"
+  }
+];
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const apiRoutes = require("./routes/apiRoutes");
+const mongoose = require("mongoose");
+
+// Serve up static assets
+app.use(express.static("client/build"));
+
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Use apiRoutes
+app.use("/api", apiRoutes);
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+// Set up promises with mongoose
+mongoose.Promise = global.Promise;
+// Connect to the Mongo DB
+mongoose.connect("mongodb://heroku_5b1b9vdv:4s7tegkehlv6bjo980lu83v9j9@ds229373.mlab.com:29373/heroku_5b1b9vdv");
+//mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/nytreact",
+  {
+    useMongoClient: true
+  }
+);
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
